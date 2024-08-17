e029a9ea8702865e5f6524ced870df3796ab0b24
yu <grace.guo012@gmail.com>
2024-08-17 11:03:34 +0100

    View/add reviews in book detail page

--- kidsbooks/views.py
+++ kidsbooks/views.py
@@ -19,8 +19,24 @@ def kidsbook_list(request):
 def kidsbook_detail(request, id):
     kidsbook = get_object_or_404(Kidsbook, id=id)
     # reviews = Review.objects.filter(kidsbook=id)
+
+    reviews = kidsbook.kidsbook_reviews.all()
+
+    # Save submitted review to the specific book and
+    # send messages after submission
+    if request.method == "POST":
+        review_form = ReviewForm(data=request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.user = request.user
+            review.book = kidsbook
+            review.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Review submitted and awaiting approval')
+    review_form = ReviewForm()
     context = {
         'kidsbook': kidsbook,
-        # 'reviews': reviews
+        'reviews': reviews,
+        'review_form': review_form
     }
     return render(request, 'kidsbooks/viewdetail.html', context)
